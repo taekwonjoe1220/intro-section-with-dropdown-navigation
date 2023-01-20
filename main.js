fefd844d3b9b14960774f7cf8e7ef496c1a6a2bd
Joe Forster <jforster.advanced@gmail.com>
2023-01-20 14:23:51 -0700

    update nav dropdown feature

--- main.js
+++ main.js
@@ -3,12 +3,26 @@ const features = document.querySelector('#features');
 const company = document.querySelector('#company');
 const featuresList = document.querySelector('#featuresList');
 const companyList = document.querySelector('#companyList');
+const featuresArrow = document.querySelector('#featuresArrow');
+const companyArrow = document.querySelector('#companyArrow');
 
 
 features.addEventListener('click', () => {
   featuresList.classList.toggle('hidden');
+  let arrowCheck = String(featuresArrow.src);
+  if (arrowCheck.includes('down')) {
+    featuresArrow.src = './images/icon-arrow-up.svg';
+  } else {
+    featuresArrow.src = './images/icon-arrow-down.svg';
+  }
 });
 company.addEventListener('click', () => {
   companyList.classList.toggle('hidden');
+  let arrowCheck = String(companyArrow.src);
+  if (arrowCheck.includes('down')) {
+    companyArrow.src = './images/icon-arrow-up.svg';
+  } else {
+    companyArrow.src = './images/icon-arrow-down.svg';
+  }
 });
 
